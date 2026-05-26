cf7901d4bb79099ceb73492c6b6cbb2c64da9379
Aditya Chatare <aditya.chatare@brightblu.com>
2026-05-26 16:30:53 +0530

    Server

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/v1/hello", (req, res) => {
+  return res.json({ data: "Hello world" }).status(200);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
